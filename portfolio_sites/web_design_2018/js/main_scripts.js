c985c9345953fda09ee6c1e6ba7960a4aa3c7497
emilgasymoff <gasimoff.emil@gmail.com>
2019-03-10 11:40:22 +0400

    Portfolio_1.04
    В примеры работ добавлена новая верстка сайта Daisy(Недоконченная начальня)ю

--- portfolio_sites/web_design_2018/js/main_scripts.js
+++ portfolio_sites/web_design_2018/js/main_scripts.js
@@ -2,3 +2,20 @@
 $(window).on('load',function(){
 	$('.preloader_area').fadeOut();
 })
+
+// SectionsSlowScroll
+$('.menu-item__link--header').click(function(){
+       $('html, body').animate({scrollTop:$('#header').position().top}, 500);
+});
+
+$('.menu-item__link--services').click(function(){
+       $('html, body').animate({scrollTop:$('#services').position().top}, 500);
+});
+
+$('.menu-item__link--portfolio').click(function(){
+       $('html, body').animate({scrollTop:$('#portfolio').position().top}, 500);
+});
+
+$('.menu-item__link--blog').click(function(){
+       $('html, body').animate({scrollTop:$('#blog').position().top}, 500);
+});
