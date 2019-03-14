9f9a8ed7bce4d8cd890acb6210b1d66cdd7461d1
emilgasymoff <gasimoff.emil@gmail.com>
2019-03-14 10:10:27 +0400

    Portfolio_1.0.7
    На главную страницу добавлены анимации появления элементов при скролле страницы.

--- js/main_script.js
+++ js/main_script.js
@@ -4,23 +4,60 @@ $(window).on('load',function(){
 })
 
 
-// SectionsSlowScroll
+// Медленный скролл при нажатии на ссылку
 $('.nav-link--mainBanner').click(function(){
-       $('html, body').animate({scrollTop:$('#mainBanner').position().top}, 500);
+       $('html, body').animate({scrollTop:$('#mainBanner').position().top}, 600);
 });
 
 $('.nav-link--aboutMe').click(function(){
-       $('html, body').animate({scrollTop:$('#aboutMe').position().top}, 500);
+       $('html, body').animate({scrollTop:$('#aboutMe').position().top}, 600);
 });
 
 $('.nav-link--usedTechnologies').click(function(){
-       $('html, body').animate({scrollTop:$('#usedTechnologies').position().top}, 500);
+       $('html, body').animate({scrollTop:$('#usedTechnologies').position().top}, 600);
 });
 
 $('.nav-link--workExamples').click(function(){
-       $('html, body').animate({scrollTop:$('#workExamples').position().top}, 500);
+       $('html, body').animate({scrollTop:$('#workExamples').position().top}, 600);
 });
 
 $('.nav-link--contacts').click(function(){
-       $('html, body').animate({scrollTop:$('#contacts').position().top}, 500);
+       $('html, body').animate({scrollTop:$('#contacts').position().top}, 600);
 });
+
+$('.orderButton').click(function(){
+       $('html, body').animate({scrollTop:$('#contacts').position().top}, 3000);
+});
+
+// Работа с WOW.js
+
+// Добавление анимации к заголовку секции и полоскам над и под ним
+
+function Heading_Horizontal_Lines_animate(){
+	$('.horizontalLine').addClass('wow fadeInRight');
+}
+Heading_Horizontal_Lines_animate();
+
+function H2_Heading_animate(){
+	$('h2.section__heading').addClass('wow fadeInLeft');
+}
+H2_Heading_animate()
+
+// Анимирование секции "Обо Мне"
+$('.infoPannel').attr('data-wow-duration','1s');
+$('.infoPannel').addClass('wow fadeInRight');
+
+// Анимирование секции "Used Technologies"
+$('.technologiesPannel').attr('data-wow-delay','0.5s');
+$('.technologiesPannel').attr('data-wow-duration','1s');
+$('.technologiesPannel').addClass('wow zoomIn');
+
+// Анимирование секции "Work Examples"
+$('.workExaples__list').attr('data-wow-duration','1s');
+$('.workExaples__list').addClass('wow slideInRight');
+
+// Добавление расстояния с низу дисплея к срабатыванию анимации
+function add_data_wow_offset(){
+	$('.wow').attr('data-wow-offset','170');
+}
+add_data_wow_offset();
